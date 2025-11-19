e8a0a1d879edf8195b5f7158b934bf221373c6a9
stoneshik <strebilly@mail.ru>
2025-11-20 00:14:47 +0300

    first

---
+++ src/main/java/avito/pr/reviewer/assignment/Application.java
@@ -0,0 +1,13 @@
+package avito.pr.reviewer.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
