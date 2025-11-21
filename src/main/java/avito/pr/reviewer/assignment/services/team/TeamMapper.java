2326f6312eb6f4a8a2f954a0ebe3631437e296af
stoneshik <strebilly@mail.ru>
2025-11-21 17:06:07 +0300

    Реализован корректный функционал teams

--- src/main/java/avito/pr/reviewer/assignment/services/team/TeamMapper.java
+++ src/main/java/avito/pr/reviewer/assignment/services/team/TeamMapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import avito.pr.reviewer.assignment.bd.entities.team.Member;
 import avito.pr.reviewer.assignment.bd.entities.team.TeamWithMembers;
+import avito.pr.reviewer.assignment.dto.requests.team.MemberInTeamAddRequestDto;
+import avito.pr.reviewer.assignment.dto.requests.team.TeamAddRequestDto;
 import avito.pr.reviewer.assignment.dto.responses.team.add.MemberInTeamAddResponseDto;
 import avito.pr.reviewer.assignment.dto.responses.team.add.TeamAddResponseDto;
 import avito.pr.reviewer.assignment.dto.responses.team.get.MemberInTeamGetResponseDto;
@@ -26,6 +28,23 @@ public class TeamMapper {
         }
         return TeamAddResponseDto.builder()
             .teamName(teamWithMembers.getTeamName())
+            .members(members)
+            .build();
+    }
+
+    public static TeamWithMembers fromTeamAddResponseDtoToTeam(TeamAddRequestDto teamAddRequestDto) {
+        List<Member> members = new ArrayList<>();
+        for (MemberInTeamAddRequestDto member: teamAddRequestDto.getMembers()) {
+            members.add(
+                Member.builder()
+                    .userId(member.getUserId())
+                    .username(member.getUsername())
+                    .isActive(member.getIsActive())
+                    .build()
+            );
+        }
+        return TeamWithMembers.builder()
+            .teamName(teamAddRequestDto.getTeamName())
             .members(members)
             .build();
     }
--- src/main/java/avito/pr/reviewer/assignment/services/team/TeamService.java
+++ src/main/java/avito/pr/reviewer/assignment/services/team/TeamService.java
@@ -1,6 +1,7 @@
 package avito.pr.reviewer.assignment.services.team;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import avito.pr.reviewer.assignment.bd.entities.team.TeamWithMembers;
 import avito.pr.reviewer.assignment.dto.requests.team.TeamAddRequestDto;
@@ -14,13 +15,16 @@ import lombok.RequiredArgsConstructor;
 public class TeamService {
     private final TeamRepository teamRepository;
 
+    @Transactional
     public TeamAddResponseDto add(TeamAddRequestDto requestDto) {
-        TeamWithMembers teamWithMembers = teamRepository.add(requestDto);
-        return TeamMapper.fromTeamToTeamAddResponseDto(teamWithMembers);
+        TeamWithMembers teamWithMembers = TeamMapper.fromTeamAddResponseDtoToTeam(requestDto);
+        TeamWithMembers savedTeamWithMembers = teamRepository.saveTeamWithUsers(teamWithMembers);
+        return TeamMapper.fromTeamToTeamAddResponseDto(savedTeamWithMembers);
     }
 
+    @Transactional
     public TeamGetResponseDto get(String teamName) {
-        TeamWithMembers teamWithMembers = teamRepository.get(teamName);
+        TeamWithMembers teamWithMembers = teamRepository.findTeamWithMembers(teamName);
         return TeamMapper.fromTeamToTeamGetResponseDto(teamWithMembers);
     }
 }
