7d3b8cd14fa47f041c5d045774876de7e3b958a1
stoneshik <strebilly@mail.ru>
2025-11-22 00:02:21 +0300

    убрал ненужный функционал

--- src/main/java/avito/pr/reviewer/assignment/repositories/PullRequestRepository.java
+++ src/main/java/avito/pr/reviewer/assignment/repositories/PullRequestRepository.java
@@ -158,34 +158,4 @@ public class PullRequestRepository {
             (rs, rowNum) -> rs.getString("user_id")
         );
     }
-
-    public List<PullRequest> getPullRequestsByAuthorId(String userId) {
-        MapSqlParameterSource selectParameterSource = new MapSqlParameterSource();
-        selectParameterSource.addValue("user_id", userId);
-        return jdbcTemplate.query(
-            """
-            SELECT
-                pull_request_id,
-                pull_request_name,
-                author_id,
-                status,
-                created_at,
-                merged_at
-            FROM pull_requests
-            WHERE author_id = :user_id;
-            """,
-            selectParameterSource,
-            (rs, rowNum) -> {
-                Timestamp mergedAt = rs.getTimestamp("merged_at");
-                return PullRequest.builder()
-                    .pullRequestId(rs.getString("pull_request_id"))
-                    .pullRequestName(rs.getString("pull_request_name"))
-                    .authorId(rs.getString("author_id"))
-                    .status(PrStatusType.valueOf(rs.getString("status")))
-                    .createdAt(rs.getTimestamp("created_at").toLocalDateTime())
-                    .mergedAt((mergedAt == null)? null : mergedAt.toLocalDateTime())
-                    .build();
-            }
-        );
-    }
 }
